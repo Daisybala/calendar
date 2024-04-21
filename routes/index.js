427d7639c934de62680393e4e8ec855ef8f32bb0
Qilian(Daisy) <qiliansong@gmail.com>
2024-04-21 01:21:43 -0700

    complete add Log In & Log Out links to the nav bar

--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Calendar' });
 });
 
-// router.get('/auth/google', passport.authenticate(
-//   // Which passport strategy is being used?
-//   'google',
-//   {
-//     // Requesting the user's profile and email
-//     scope: ['profile', 'email'],
-//     // Optionally force pick account every time
-//     // prompt: "select_account"
-//   }
-// ));
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
 
-// router.get('/oauth2callback', passport.authenticate(
-//   'google',
-//   {
-//     successRedirect: '?',
-//     failureRedirect: '?'
-//   }
-// ));
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
 
-// router.get('/logout', function(req, res){
-//   req.logout(function() {
-//     res.redirect('/movies');
-//   });
-// });
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
