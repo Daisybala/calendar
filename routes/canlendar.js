e263b96ea011950b6f87c26b7b2e6aa086c161cb
Qilian(Daisy) <qiliansong@gmail.com>
2024-04-22 14:19:10 -0700

    Revert "add css to homepage and add links to navigate to the previous and next months"
    This reverts commit d6b0c98d1f7d44ccdf8232c0001c6bfc52a41a32.

--- routes/canlendar.js
+++
@@ -1,6 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const passport = require('passport');
-// const user = require('../models/user');
-
-// // GET /calendar
