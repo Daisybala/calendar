69ac832c78aeef4abc367b2a4753feee1c728cfe
Qilian(Daisy) <qiliansong@gmail.com>
2024-04-24 14:28:10 -0700

    complete edit functionaly

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,22 +1,24 @@
 const Todo = require("../models/todo");
+const Category = require('../models/category');
 
 module.exports = {
     create,
-    delete: deleteTodo
+    delete: deleteTodo,
+    edit
 };
+
+async function edit(req, res) {
+  const todo = await Todo.findById(req.params.id);
+  const categories = await Category.find({});
+  res.render('todos/edit', { todo, categories})
+}
 
 async function deleteTodo(req, res) {
   const todo = await Todo.findOneAndDelete({ _id: req.params.id, user: req.user._id });
+  if (!todo) return res.redirect('/');
   const date = todo.date;
   res.redirect(`/year/${date.getFullYear()}/month/${date.getMonth()}/day/${date.getDate()}`);
 }
-// async function deleteTodo(req, res) {
-//   const todo = await Todo.findOne({ 'todos._id': req.params.id, 'todos.user': req.user._id});
-//   console.log(req.body);
-//   Todo.deleteOne(req.params.id);
-//   const date = new Date(req.body.date);
-//   res.redirect(`/year/${date.getFullYear()}/month/${date.getMonth()}/day/${date.getDate()}`);
-// }
 
 async function create(req, res) {
     // Add the user-centric info to req.body (the new review)
--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const todosCtrl = require('../controllers/todos');
 
-
-// // DELETE /todos/:id
+// GET /todos/:id/edit
+router.get('/:id/edit', todosCtrl.edit);
+// DELETE /todos/:id
 router.delete('/:id', todosCtrl.delete);
 // POST /todos
 router.post('/', todosCtrl.create);
-
+// PUT /todos/:id
 
 module.exports = router;
